e14e8258a4868d7d9dd62225d1a2ad24fd98444a
Manav Patel <mpatel2@albany.edu>
2019-01-05 08:48:40 -0500

    first commit

---
+++ src/main/java/com/example/demo/UserRepo.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.List;
+//import java.util.Optional;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepo extends CrudRepository<Users, String> {
+	public List<Users> findAll();
+	public void  deleteById(String id);
+	public List<Users> findByName(String name);
+	public Optional<Users> findById(String id);
+	
+}
+
+
